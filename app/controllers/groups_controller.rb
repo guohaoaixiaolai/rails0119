04322fde51bd6075f45218df9d87a22f3965b060
grithope <zhunti99999@gmail.com>
2017-01-20 11:50:28 +0800

    ch06

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,7 +15,8 @@ def create
   @group.user = current_user
 
   if @group.save
-redirect_to groups_path
+    current_user.join!(@group)
+    redirect_to groups_path
   else
   render :new
   end
@@ -46,6 +47,29 @@ def destroy
   @group.destroy
   flash[:alert] = "Group deleted"
   redirect_to groups_path
+end
+
+def join
+  @group = Group.find(params[:id])
+  if !current_user.is_member_of?(@group)
+    current_user.join!(@group)
+    flash[:notice] = "加入本讨论版成功"
+  else
+    flash[:warning] = "你已经不是本讨论版成员了"
+  end
+  redirect_to group_path(@group)
+end
+
+
+def quit
+@group = Group.find(params[:id])
+if current_user.is_member_of?(@group)
+  current_user.quit!(@group)
+  flash[:alert] = "已退出本讨论版"
+else
+  flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+end
+redirect_to group_path(@group)
 end
 
 private
